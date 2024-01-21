5051f20f75ad7f62322cd25a2f73b72cb3bc077f
Ahd-3hd <ahdhani0@gmail.com>
2024-01-21 14:17:47 +0300

    fix: rename files

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { login, register, verifyToken } from "../controllers/auth.controller";
-import { validate } from "../middlewares";
+import { validate } from "../middlewares/validate";
 import { regiserSchema } from "../middlewares/schemas/register.schema";
 import { loginSchema } from "../middlewares/schemas/login.schema";
 
